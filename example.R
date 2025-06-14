a6b6910229a0791b85b13daa1cc42166ec78838a
luc-azen <lazen13@verizon.net>
2025-06-13 20:22:44 -0400

    Add managerial season analysis

--- example.R
+++ example.R
@@ -1,4 +1,16 @@
-setwd("C:/Users/lazen/OneDrive/Documents/GitHub/example_codex_project")
+# Example script using Lahman data
 
 library(tidyverse)
+library(Lahman)
 library(baseballr)
+
+# top 50 winningest managerial seasons since 1960
+manager_seasons <- Managers %>%
+  filter(yearID >= 1960) %>%
+  left_join(Teams %>% select(yearID, teamID, team_name = name, WSWin), by = c("yearID", "teamID")) %>%
+  left_join(People %>% select(playerID, nameFirst, nameLast), by = "playerID") %>%
+  mutate(manager = paste(nameFirst, nameLast),
+         wonWS = WSWin == "Y") %>%
+  arrange(desc(W)) %>%
+  select(yearID, manager, team_name, W, wonWS) %>%
+  slice_head(n = 50)
